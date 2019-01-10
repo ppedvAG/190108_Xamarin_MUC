88a450abdf856e6533cf73da236b16cdc7af26cb
Michael Zöhling <Michael.Zoehling@outlook.com>
2019-01-10 11:21:04 +0100

    MVVM_Demo

---
+++ MVVM_Demo/MVVM_Demo/MVVM_Demo/Services/PersonenService.cs
@@ -0,0 +1,27 @@
+﻿using MVVM_Demo.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MVVM_Demo.Services
+{
+    class PersonenService
+    {
+        public List<Person> GetPersonen()
+        {
+            return new List<Person>
+            {
+                new Person{Vorname="Tom",Nachname="Ate",Alter=10,Kontostand=100m},
+                new Person{Vorname="Anna",Nachname="Nass",Alter=20,Kontostand=2000m},
+                new Person{Vorname="Peter",Nachname="Silie",Alter=30,Kontostand=-3000m},
+                new Person{Vorname="Franz",Nachname="Ose",Alter=40,Kontostand=0m},
+                new Person{Vorname="Martha",Nachname="Pfahl",Alter=50,Kontostand=5555200m},
+                new Person{Vorname="Klara",Nachname="Fall",Alter=60,Kontostand=-212300m},
+                new Person{Vorname="Rainer",Nachname="Zufall",Alter=70,Kontostand=70m},
+                new Person{Vorname="Bill",Nachname="Dung",Alter=80,Kontostand=25678900m},
+                new Person{Vorname="Anna",Nachname="Bolika",Alter=90,Kontostand=1111200m},
+                new Person{Vorname="Axel",Nachname="Schweiß",Alter=100,Kontostand=-0.99m}
+            };
+        }
+    }
+}
---
+++ MVVM_Demo/MVVM_Demo/MVVM_Demo/ViewModels/MainViewModel.cs
@@ -0,0 +1,36 @@
+﻿using MVVM_Demo.Models;
+using MVVM_Demo.Services;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Text;
+using Xamarin.Forms;
+
+namespace MVVM_Demo.ViewModels
+{
+    class MainViewModel : BaseViewModel
+    {
+        public MainViewModel()
+        {
+            // Controllfreak-Antipattern
+            service = new PersonenService();
+            GetPersonenCommand = new Command(GetPersonen);
+        }
+
+        private void GetPersonen(object obj)
+        {
+            Personenliste = service.GetPersonen();
+        }
+
+        private readonly PersonenService service;
+
+        private List<Person> personenliste;
+        public List<Person> Personenliste
+        {
+            get => personenliste;
+            set => SetProperty(ref personenliste, value);
+        }
+        public Command GetPersonenCommand { get; set; }
+    }
+}
