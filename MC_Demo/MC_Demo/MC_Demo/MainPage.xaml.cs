7b388151853384130a5483ac5d4950f9c9fc2f8b
Michael Zöhling <Michael.Zoehling@outlook.com>
2019-01-10 17:00:38 +0100

    MC

---
+++ MC_Demo/MC_Demo/MC_Demo/MainPage.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace MC_Demo
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+            MessagingCenter.Subscribe<Slider, double>(this, "SliderValueChanged", NachrichtVomSlider);
+        }
+
+        private void NachrichtVomSlider(Slider arg1, double arg2)
+        {
+            entryWertAusSlider.Text = arg2.ToString();
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            Navigation.PushModalAsync(new Seite2());
+        }
+    }
+}
---
+++ MC_Demo/MC_Demo/MC_Demo/Seite2.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace MC_Demo
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class Seite2 : ContentPage
+	{
+		public Seite2 ()
+		{
+			InitializeComponent ();
+		}
+
+        private void Slider_ValueChanged(object sender, ValueChangedEventArgs e)
+        {
+            MessagingCenter.Send(sender as Slider, "SliderValueChanged", e.NewValue);
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            Navigation.PopModalAsync();
+        }
+    }
+}
